0b32518fb91c05756d58c2d82a43f9b4cdd9b425
sanyam_git <jatalesanyam7@gmail.com>
2021-06-03 14:45:54 +0530

    Added fn headers

--- math.py
+++ math.py
@@ -1,11 +1,15 @@
+#Add Implementation
 def add(x,y):
     pass
 
+#Subtract implementation
 def subtract(x,y):
     pass
 
+#Multiply Implementation
 def multiply(x,y):
     pass
 
+#Divsion Implementation
 def divide(x,y):
     pass
